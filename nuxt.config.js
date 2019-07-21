c7d0e7f1c2e1af085a078a6dad1d3ed480930fc3
Sven Franck <svenfranck@yahoo.com>
2019-07-21 11:00:22 +0200

    add basic i18n support

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,30 @@
 export default {
-    plugins: ['~plugins/components']
-}
+  plugins: ["~plugins/components"],
+  modules: [
+    [
+      "nuxt-i18n",
+      {
+        locales: [
+          {
+            name: "Dutch",
+            code: "nl",
+            iso: "nl-NL",
+            file: "ui/nl-NL.js"
+          },
+          {
+            name: "English",
+            code: "en",
+            iso: "en-US",
+            file: "ui/en-US.js"
+          }
+        ],
+        lazy: true,
+        strategy: "prefix_except_default",
+        loadLanguagesAsync: true,
+        langDir: "pages/_lang/",
+        defaultLocale: "nl",
+        parsePages: false
+      }
+    ]
+  ]
+};
