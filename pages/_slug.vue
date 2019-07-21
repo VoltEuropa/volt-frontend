3099470ed1182aabefe577b7b4248466d06d6ca1
Tom Ootes <tomootes@gmail.com>
2019-07-21 09:47:35 +0200

    added site to homepage

---
+++ pages/_slug.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>
+    <h1>{{page.title}}</h1>
+    <p>{{$store.state.site.site}}</p>
+    <ul>
+        <li v-for="page in pages" v-bind:key="page.slug">{{page}}</li>
+    </ul>
+  </h1>
+
+</template>
+<script>
+export default {
+
+  async fetch({ store }) {
+    return store.dispatch("pages/get")
+      .then( store.dispatch("site/get") )
+  },
+
+  created () {
+      console.log(this.$store);
+  },
+
+  computed: {
+    page() {
+        return  this.$store.getters['pages/getBySlug'](this.$route.params.slug);
+    },
+    site() {
+        return this.$store.site.site
+    }
+  }
+
+
+};
+</script>
